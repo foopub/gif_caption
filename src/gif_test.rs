6c82521aed8ea2a79a5ef0e451e1bb5edd679138
foo <krowlaxs@gmail.com>
2021-09-25 23:34:39 +0100

    Gifs work with blank extension

---
+++ src/gif_test.rs
@@ -0,0 +1,17 @@
+use std::fs::File;
+use std::io::Write;
+
+use crate::gif_processor;
+
+#[test]
+fn sample_gifs()
+{
+    let input = File::open("test.gif").unwrap();
+    let mut out_image = File::create("result.gif").unwrap();
+    let out = gif_processor::caption(
+        &String::new(),
+        input,
+        &String::new(),
+    );
+    out_image.write(&out).unwrap();
+}
