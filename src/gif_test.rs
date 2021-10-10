67decc480fd8095945827272f01b14b55fcd51a4
foo <krowlaxs@gmail.com>
2021-10-10 23:13:30 +0100

    Don't need spaghetti test anymore

--- src/gif_test.rs
+++ src/gif_test.rs
@@ -1,11 +1,7 @@
 use std::fs::File;
 use std::io::{Read, Write};
-use std::iter::FromIterator;
 
-use gif::{ColorOutput, DecodeOptions, Encoder, Repeat};
-use rgb::RGB;
-
-use crate::{clustering, gif_processor};
+use crate::gif_processor;
 
 #[test]
 fn sample_gifs()
@@ -26,96 +22,4 @@ fn sample_gifs()
         //Some(60.0),
     );
     out_image.write(&out).unwrap();
-}
-
-#[test]
-fn wu_algo()
-{
-    let mut buf = Vec::new();
-    {
-        let mut input = File::open("test.gif").unwrap();
-        input.read_to_end(&mut buf).unwrap();
-    }
-    let mut decoder = {
-        let mut options = DecodeOptions::new();
-        options.set_color_output(ColorOutput::RGBA);
-        options.read_info(buf.as_slice()).unwrap()
-    };
-
-    let mut all_colours = Vec::new();
-
-    if let Some(p) = &decoder.global_palette() {
-        all_colours.extend(pallette_to_rgb(p));
-    }
-    while let Some(frame) = decoder.read_next_frame().unwrap() {
-        if let Some(p) = &frame.palette {
-            all_colours.extend(pallette_to_rgb(p));
-        }
-    }
-
-    let (colours_flat, indices) = clustering::compress(all_colours, 256);
-    //all_colours.dedup();
-    //let mut sorted = pallette_to_rgb(&colours_flat);
-    //sorted.sort_unstable();
-    //for palettes of 256 or less colours, these should be the same!
-    //println!("All colours:\n{:?}", all_colours);
-    //println!("Reduced palette:\n{:?}", sorted);
-    //println!("All colours:\n{:?}", all_colours.len());
-    //println!("Reduced palette:\n{:?}", colours_flat.len());
-    //drop(all_colours);
-
-    let mut out_image = File::create("xresult.gif").unwrap();
-
-    let mut encoder = Encoder::new(
-        &mut out_image,
-        decoder.width(),
-        decoder.height(),
-        &colours_flat,
-    )
-    .unwrap();
-    encoder.set_repeat(Repeat::Infinite).unwrap();
-
-    //need to repeat this
-    let mut decoder = {
-        let mut options = DecodeOptions::new();
-        options.set_color_output(ColorOutput::RGBA);
-        options.read_info(buf.as_slice()).unwrap()
-    };
-
-    while let Some(old_frame) = decoder.read_next_frame().unwrap() {
-        let mut new_frame = old_frame.clone();
-
-        new_frame.palette = None;
-
-        let triplets: Vec<[u8; 3]> = old_frame
-            .buffer
-            .chunks_exact(4)
-            .map(|x| [x[0], x[1], x[2]])
-            .collect();
-
-        let mut new_buff = Vec::with_capacity(triplets.len());
-
-        triplets.iter().for_each(|x| {
-            //let i = indices[round(x[0])][round(x[1])][round(x[2])];
-            //println!("{}, {:?}, {:?}", i, x, sorted[i as usize]);
-            new_buff.push(*indices.index(RGB::new(
-                x[0] >> 3,
-                x[1] >> 3,
-                x[2] >> 3,
-            )));
-        });
-        //println!("{}", new_buff.len());
-
-        new_frame.buffer = new_buff.into();
-        encoder.write_frame(&new_frame).unwrap();
-    }
-    drop(encoder);
-}
-
-fn pallette_to_rgb(palette: &[u8]) -> Vec<RGB<u8>>
-{
-    palette
-        .chunks(3)
-        .map(|x| RGB::from_iter(x.to_owned()))
-        .collect()
 }
